38b68eeca1c4993ffb5a24f37562fc73db0aeaf0
AbsintheSung <h90243768@gmail.com>
2025-02-22 17:15:48 +0800

    init:專案建立&初始化

---
+++ eslint.config.ts
@@ -0,0 +1,32 @@
+import pluginVue from 'eslint-plugin-vue'
+import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript'
+import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
+
+// To allow more languages other than `ts` in `.vue` files, uncomment the following lines:
+// import { configureVueProject } from '@vue/eslint-config-typescript'
+// configureVueProject({ scriptLangs: ['ts', 'tsx'] })
+// More info at https://github.com/vuejs/eslint-config-typescript/#advanced-setup
+
+export default defineConfigWithVueTs(
+  {
+    name: 'app/files-to-lint',
+    files: ['**/*.{ts,mts,tsx,vue}'],
+  },
+
+  {
+    name: 'app/files-to-ignore',
+    ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**'],
+  },
+  pluginVue.configs['flat/essential'],
+  vueTsConfigs.recommended,
+  skipFormatting,
+  {
+    rules: {
+      "vue/block-lang": ["error", {
+        "script": "never",   // 不強制要求 <script> 的 lang
+        "style": "always",   // <style> 必須指定 lang，例如 lang="scss"
+        // "template": "always" // <template> 必須指定 lang
+      }]
+    }
+  }
+)
